02fd532db419427f0460af3dcabc50d34f3969fb
Leonardo Piovesan <leopvsn@gmail.com>
2020-08-27 12:25:36 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+window.addEventListener('load', () =>{
+    const sounds = document.querySelectorAll('.sound');
+    const pads = document.querySelectorAll('.pads div');
+    const visual = document.querySelector('.visual')
+    const colors = [
+        '#78d1c5',
+        '#d36060',
+        '#d360d3',
+        '#c7d360',
+        '#8ad360',
+        '#6260d3'
+    ];
+    //Lets get going with the sound here
+    pads.forEach((pad, index) =>{
+        pad.addEventListener('click', function(){
+            sounds[index].currentTime = 0;
+            sounds[index].play();
+
+            createBubbles(index);
+        });
+    });
+    //Create a function that makes bubbles
+    const createBubbles = (index) => {
+        const bubble = document.createElement('div');
+        visual.appendChild(bubble);
+        bubble.style.backgroundColor = colors[index];
+        bubble.style.animation = 'jump 1s ease'
+        bubble.addEventListener('animationend', function(){
+            visual.removeChild(this);
+        });
+    };
+});
